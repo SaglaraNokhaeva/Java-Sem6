7355bafc15e2d053056602398aa77f6ec2c702ae
Saglara <nohaevass@yandex.ru>
2023-02-07 21:12:19 +0300

    Update task1.java

--- task1.java
+++ task1.java
@@ -9,9 +9,12 @@ import java.util.*;
 public class task1 {
     public static void main(String[] args) {
         // var nums = new HashSet<>(Arrays.asList(1, 200, 3, 2, 4000, 5, 6, 3));
+        //ХэшСет хранит очередность в своем порядке
         Set <Integer> nums = new HashSet<>(Arrays.asList(1, 200, 3, 2, 4000, 5, 6, 3));
         System.out.println(nums);
 
+        
+        //ЛинкетСет сохраняет очередность ввода
         var LinkSet = new LinkedHashSet<>(Arrays.asList(1, 7, 200, 3, 6, 4, 5));
         System.out.println(LinkSet);
     }
